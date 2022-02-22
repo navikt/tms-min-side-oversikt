c3e6745fc899522e9d1aeabffc908ee16b72956f
eivinne <eivinn_e@hotmail.com>
2022-02-22 16:18:28 +0100

    Trigger ny commit for å sjekke workflow

--- src/components/Komponent.jsx
+++ src/components/Komponent.jsx
@@ -6,7 +6,7 @@ const Komponent = ({ tekst }) => {
   return (
     <div className="komponent">
       <Panel>
-        <p>{tekst} Hello World x2!</p>
+        <p>{tekst} Hello World x3!</p>
       </Panel>
     </div>
   );

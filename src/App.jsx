c4e3ebfb40cbaf1cb0df6ec3e4efbd44aca7e50b
eivinne <eivinn_e@hotmail.com>
2022-04-25 13:43:06 +0200

    Fjernet en feilaktig import

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { fetcher } from "./api/api";
 import MainComponent from "./components/MainComponent/MainComponent";
 import "@navikt/ds-css";
 import "./App.css";
-import "./components/mainComponent/MainComponent.css";
 
 function App() {
   const { data } = useQuery(apiUrl, fetcher);

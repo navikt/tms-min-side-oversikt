b4d16be692120e922e6c18e9d79cb5ae004515c8
eivinne <eivinn_e@hotmail.com>
2022-04-25 14:11:11 +0200

    Fjernet ubrukte imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import { useQuery } from "react-query";
-import { apiUrl } from "./api/urls";
-import { fetcher } from "./api/api";
 import MainComponent from "./components/MainComponent/MainComponent";
 import "@navikt/ds-css";
 import "./App.css";
 
 function App() {
-  const { data } = useQuery(apiUrl, fetcher);
-
   return (
     <main className="main">
       <div className="app">

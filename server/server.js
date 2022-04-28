c4d8feff16ac9e312631d44bcfcd5eccd05010eb
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-04-28 17:18:29 +0200

    Henter in cors allowed origin fra secrets

--- server/server.js
+++ server/server.js
@@ -4,8 +4,9 @@ const path = require("path");
 const basePath = "/tms-min-side-oversikt";
 const buildPath = path.resolve(__dirname, "../dist");
 const server = express();
+const corsAllowedOrigin = process.env.CORS_ALLOWED_ORIGIN || "http://localhost:3000";
 
-server.use(cors({ origin: "http://localhost:3000" }));
+server.use(cors({ origin: corsAllowedOrigin }));
 
 server.use(basePath, express.static(buildPath));
 

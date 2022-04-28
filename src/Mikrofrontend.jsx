c74ced579ba9b7d13e24913fb23dee79a19b71e1
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-04-28 17:36:15 +0200

    Slår av refetching av queries

--- src/Mikrofrontend.jsx
+++ src/Mikrofrontend.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClientProvider } from "react-query";
 import LanguageProvider from "./providers/LanguageProvider";
+import queryClient from "./utils/query";
 import App from "./App";
 import "./App.css";
 
 const Mikrofrontend = () => {
   return (
     <LanguageProvider defaultSprak="nb">
-      <QueryClientProvider client={new QueryClient()}>
+      <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
     </LanguageProvider>

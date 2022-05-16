6be8fae2e9432a45732f369a83dcc6812ad44ceb
eivinne <eivinn_e@hotmail.com>
2022-05-16 13:19:17 +0200

    La inn de generelle flisene fra dittnav

---
+++ src/components/generelleFliser/Flis.jsx
@@ -0,0 +1,30 @@
+import { useIntl } from "react-intl";
+import { LinkPanel } from "@navikt/ds-react";
+import "./Flis.css";
+
+const Flis = ({ tittel, ingress, ikon, href }) => {
+  const translate = useIntl();
+
+  return (
+    <>
+      <LinkPanel href={href} border={false}>
+        <div
+          style={{
+            display: "grid",
+            gridAutoFlow: "column",
+            gap: "var(--navds-spacing-8)",
+            alignItems: "center",
+          }}
+        >
+          {ikon}
+          <div>
+            <LinkPanel.Title>{translate.formatMessage({ id: tittel })}</LinkPanel.Title>
+            <LinkPanel.Description>{translate.formatMessage({ id: ingress })}</LinkPanel.Description>
+          </div>
+        </div>
+      </LinkPanel>
+    </>
+  );
+};
+
+export default Flis;

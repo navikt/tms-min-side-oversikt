d204fc99314527db506f3a2f7438ca1950484921
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2022-08-16 15:39:53 +0200

    Sett opp a11y linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,13 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:jsx-a11y/recommended"],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["react"],
+  plugins: ["react", "jsx-a11y"],
   ignorePatterns: [".eslintrc.js", "**/dist/*.js", "vite.config.js", "/server/server.js"],
   rules: {
     indent: ["error", "tab"],
@@ -21,5 +21,8 @@ module.exports = {
     "react/react-in-jsx-scope": "off",
     "react-in-jsx-scope": "off",
     "react/jsx-key": "warn",
+    "no-mixed-spaces-and-tabs": "off",
+    "jsx-a11y/click-events-have-key-events": "warn",
+    "jsx-a11y/no-static-element-interactions": "warn",
   },
 };

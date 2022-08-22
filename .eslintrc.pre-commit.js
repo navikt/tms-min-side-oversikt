67f3ffc285faf2ca3f1547b137ba9328367cd66e
eivinne <eivinn_e@hotmail.com>
2022-08-22 12:38:10 +0200

    Endret på regelsettet for linting ved commit.

--- .eslintrc.pre-commit.js
+++ .eslintrc.pre-commit.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: [".eslintrc.js"],
   rules: {
     //TODO: enable rules
-    "no-unused-vars": "off",
     "react/prop-types": "off",
     "react-in-jsx-scope": "off",
     "react/jsx-key": "warn",

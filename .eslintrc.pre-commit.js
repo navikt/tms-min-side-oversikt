9e5f59097a7b939aa220f48c3ec55426d3edd1cd
eivinne <eivinn_e@hotmail.com>
2022-08-22 12:35:43 +0200

    Test av linting

--- .eslintrc.pre-commit.js
+++ .eslintrc.pre-commit.js
@@ -7,8 +7,5 @@ module.exports = {
     "react-in-jsx-scope": "off",
     "react/jsx-key": "warn",
     "no-mixed-spaces-and-tabs": "off",
-    "jsx-a11y/click-events-have-key-events": "warn",
-    "jsx-a11y/no-static-element-interactions": "warn",
-    "jsx-a11y/anchor-is-valid": "warn",
   },
 };

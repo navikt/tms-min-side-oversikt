31d0ab48fec59864e0bfdb371c1a0ba4122bfdcc
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-09-28 10:41:10 +0200

    Henter inn en mikrofrontend for meldekort

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import React from "react";
 import Sidetittel from "./components/sidetittel/Sidetittel";
 import VarslerList from "./components/varsler/varsler-list/VarslerList";
 import InngangVarslinger from "./components/inngang-varslinger/InngangVarslinger";
-import "@navikt/ds-css";
 import Meldekort from "./components/meldekort/Meldekort";
 import EtterregistreringMeldekort from "./components/meldekort/etterregistrering/EtterregistreringMeldekort";
+import { meldekortMikrofrontendUrl } from "./api/urls";
+import "@navikt/ds-css";
 import CSS from "./App.module.css";
+
+const MeldekortMikrofrontend = React.lazy(() => import(meldekortMikrofrontendUrl));
 
 function App() {
   return (
@@ -13,6 +17,7 @@ function App() {
         <section className="page-wrapper-microfrontend">
           <Sidetittel />
           <ul className={CSS.varsler}>
+            <MeldekortMikrofrontend />
             <Meldekort />
             <EtterregistreringMeldekort />
             <VarslerList />

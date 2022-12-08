0024f6e344ec5024c1c8aa5981336905ef7d0a0d
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-12-08 18:35:23 +0100

    Heading for varsler

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,25 @@ import VarslerList from "./components/varsler/varsler-list/VarslerList";
 import InngangVarslinger from "./components/inngang-varslinger/InngangVarslinger";
 import { meldekortMikrofrontendUrl } from "./api/urls";
 import ErrorBoundary from "./components/error-boundary/ErrorBoundary";
+import Utkast from "./components/utkast/Utkast";
+import { Heading } from "@navikt/ds-react";
+import { useIntl } from "react-intl";
 import "@navikt/ds-css";
 import CSS from "./App.module.css";
-import Utkast from "./components/utkast/Utkast";
 
 const MeldekortMikrofrontend = React.lazy(() => import(meldekortMikrofrontendUrl));
 
 function App() {
+  const { formatMessage } = useIntl();
+
   return (
     <main className={CSS.main}>
       <div className={CSS.app}>
         <section className="page-wrapper-microfrontend">
           <Sidetittel />
+          <Heading size="medium" level="2" spacing>
+            {formatMessage({ id: "varsler.tittel" })}
+          </Heading>
           <ul className={CSS.varsler}>
             <React.Suspense fallback={null}>
               <ErrorBoundary>

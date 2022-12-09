69222d61a0413929701e0f01deb58284ee02bb0a
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-12-09 16:12:09 +0100

    Heading for varsler

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import Sidetittel from "./components/sidetittel/Sidetittel";
-import VarslerList from "./components/varsler/varsler-list/VarslerList";
+import Varsler from "./components/varsler/varsler/Varsler.jsx";
 import InngangVarslinger from "./components/inngang-varslinger/InngangVarslinger";
 import { meldekortMikrofrontendUrl } from "./api/urls";
 import ErrorBoundary from "./components/error-boundary/ErrorBoundary";
 import Utkast from "./components/utkast/Utkast";
-import { Heading } from "@navikt/ds-react";
-import { useIntl } from "react-intl";
-import "@navikt/ds-css";
 import CSS from "./App.module.css";
+import "@navikt/ds-css";
 
 const MeldekortMikrofrontend = React.lazy(() => import(meldekortMikrofrontendUrl));
 
 function App() {
-  const { formatMessage } = useIntl();
-
   return (
     <main className={CSS.main}>
       <div className={CSS.app}>
@@ -25,14 +21,7 @@ function App() {
               <MeldekortMikrofrontend />
             </ErrorBoundary>
           </React.Suspense>
-          <section>
-            <Heading size="medium" level="2" spacing>
-              {formatMessage({ id: "varsler.tittel" })}
-            </Heading>
-            <ul className={CSS.varsler}>
-              <VarslerList />
-            </ul>
-          </section>
+          <Varsler />
           <InngangVarslinger />
           <Utkast />
         </div>
